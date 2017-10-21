00844ff8e74f5c50ef48edd63f6a8cae0f108e01
max thomas <max@maxthomas.io>
2017-10-21 12:58:54 -0300

    support redis password for fetch; ping redis on init

--- cmd/gretchfetch/main.go
+++ cmd/gretchfetch/main.go
@@ -17,26 +17,31 @@ var (
 
 func main() {
 	var (
-		redisServer = pflag.String("redis-server", "localhost", "redis server to connect to")
-		redisPort   = pflag.Int("redis-port", 6379, "redis port to connect to")
-		fetchServer = pflag.String("fetch-server", "localhost", "fetch server to listen on")
-		fetchPort   = pflag.Int("fetch-port", 9099, "fetch port to listen on")
+		redisServer        = pflag.String("redis-server", "localhost", "redis server to connect to")
+		redisPort          = pflag.Int("redis-port", 6379, "redis port to connect to")
+		redisPassword      = pflag.String("redis-password", "", "redis password")
+		fetchServerAddress = pflag.String("fetch-server", "localhost:9099", "fetch address host:port")
 	)
 
 	pflag.Parse()
-	if *redisServer == "" || *fetchServer == "" {
+	if *redisServer == "" {
 		pflag.Usage()
 		return
 	}
 
 	redisCfg := &redis.Options{Addr: *redisServer + strconv.Itoa(*redisPort)}
+	if *redisPassword != "" {
+		redisCfg.Password = *redisPassword
+	}
 	redisCli := redis.NewClient(redisCfg)
 	defer redisCli.Close()
+	if err := redisCli.Ping().Err(); err != nil {
+		logger.Fatal("failed redis connect", zap.Error(err))
+	}
 	logger.Info("connected to redis")
 
 	gr := gretchin.NewGretchin(redisCli)
-	srvString := *fetchServer + ":" + strconv.Itoa(*fetchPort)
-	socket, err := thrift.NewTServerSocket(srvString)
+	socket, err := thrift.NewTServerSocket(*fetchServerAddress)
 	if err != nil {
 		logger.Fatal("error during socket setup", zap.Error(err))
 	}
@@ -45,7 +50,7 @@ func main() {
 	protoFactory := gretchin.DefaultProtocolFactory()
 	proc := goncrete.NewFetchCommunicationServiceProcessor(gr)
 	srvr := thrift.NewTSimpleServer4(proc, socket, transFactory, protoFactory)
-	logger.Info("server preparing to serve", zap.String("address", srvString))
+	logger.Info("server preparing to serve", zap.String("address", *fetchServerAddress))
 	if err = srvr.Serve(); err != nil {
 		logger.Error("Error during server", zap.Error(err))
 	}
